5ecb0b70e49519b0d75d0b59ded549428869f208
luts <lutsjaroslav@gmail.com>
2026-09-18 15:31:16 +0300

    fix(frontend): handle API network failures correctly

--- frontend/hbapp/mixins/apiQueriesHelper.js
+++ frontend/hbapp/mixins/apiQueriesHelper.js
@@ -19,71 +19,54 @@ const api = {
 		return requestHeaders
 	},
 
-	get: function(path, headers = {}) {
-		return new Promise(async (resolve, reject) => {
-			const requestData = {
-				method: 'GET',
-				headers: this.getHeaders(headers)
-			}
-			const responce = await fetch(this.apiurl + path, requestData)
-			if ([200, 201].indexOf(responce.status) !== -1) {
-				const resjson = await responce.json()
-				resolve(resjson)
-			} else {
-				reject(1)
-			}
-
-		})
+	get: async function(path, headers = {}) {
+		const requestData = {
+			method: 'GET',
+			headers: this.getHeaders(headers)
+		}
+		const responce = await fetch(this.apiurl + path, requestData)
+		if ([200, 201].indexOf(responce.status) !== -1) {
+			return responce.json()
+		}
+		throw 1
 	},
 
-	post: function(path, data, headers = {}) {
-		return new Promise(async (resolve, reject) => {
-			const requestData = {
-				method: 'POST',
-				headers: this.getHeaders(headers)
-			}
-			if (data) requestData.body = JSON.stringify(data)
-			const responce = await fetch(this.apiurl + path, requestData)
-			if ([200, 201].indexOf(responce.status) !== -1) {
-				const resjson = await responce.json()
-				resolve(resjson)
-			} else {
-				reject(1)
-			}
-		})
+	post: async function(path, data, headers = {}) {
+		const requestData = {
+			method: 'POST',
+			headers: this.getHeaders(headers)
+		}
+		if (data) requestData.body = JSON.stringify(data)
+		const responce = await fetch(this.apiurl + path, requestData)
+		if ([200, 201].indexOf(responce.status) !== -1) {
+			return responce.json()
+		}
+		throw 1
 	},
 
-	put: function(path, data, headers = {}) {
-		return new Promise(async (resolve, reject) => {
-			const requestData = {
-				method: 'PUT',
-				headers: this.getHeaders(headers)
-			}
-			if (data) requestData.body = JSON.stringify(data)
-			const responce = await fetch(this.apiurl + path, requestData)
-			if ([200, 201].indexOf(responce.status) !== -1) {
-				const resjson = await responce.json()
-				resolve(resjson)
-			} else {
-				reject(1)
-			}
-		})
+	put: async function(path, data, headers = {}) {
+		const requestData = {
+			method: 'PUT',
+			headers: this.getHeaders(headers)
+		}
+		if (data) requestData.body = JSON.stringify(data)
+		const responce = await fetch(this.apiurl + path, requestData)
+		if ([200, 201].indexOf(responce.status) !== -1) {
+			return responce.json()
+		}
+		throw 1
 	},
 
-	delete: function(path, headers = {}) {
-		return new Promise(async (resolve, reject) => {
-			const requestData = {
-				method: 'DELETE',
-				headers: this.getHeaders(headers)
-			}
-			const responce = await fetch(this.apiurl + path, requestData)
-			if ([200, 201].indexOf(responce.status) !== -1) {
-				const resjson = await responce.json()
-				resolve(resjson)
-			} else {
-				reject(1)
-			}
-		})
+	delete: async function(path, headers = {}) {
+		const requestData = {
+			method: 'DELETE',
+			headers: this.getHeaders(headers)
+		}
+		const responce = await fetch(this.apiurl + path, requestData)
+		if ([200, 201].indexOf(responce.status) !== -1) {
+			return responce.json()
+		}
+		throw 1
 	}
 }
 
