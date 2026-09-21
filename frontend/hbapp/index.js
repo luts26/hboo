02b0ad7258432122565c76fd89a01315898d3a8b
luts <lutsjaroslav@gmail.com>
2026-09-21 23:10:25 +0300

    pwa: test fullscreen mode

--- frontend/hbapp/index.js
+++ frontend/hbapp/index.js
@@ -1,5 +1,11 @@
 import hbapp from './hbapp.js'
 import {registerServiceWorker} from './services/ServiceWorkerRegistration.js'
 
+alert(
+    'fullscreen: ' + window.matchMedia('(display-mode: fullscreen)').matches +
+    '\nstandalone: ' + window.matchMedia('(display-mode: standalone)').matches +
+    '\nbrowser: ' + matchMedia('(display-mode: browser)').matches
+);
+
 hbapp.createProject()
 registerServiceWorker()
